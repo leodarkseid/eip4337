27623bed63a1d2f225e5c1fd30b26ef457253897
Leo <leodarkseid@gmail.com>
2023-06-23 19:03:18 +0100

    eip4337 from stackup

---
+++ scripts/simpleAccount/address.ts
@@ -0,0 +1,14 @@
+import { ethers } from "ethers";
+import { Presets } from "userop";
+// @ts-ignore
+import config from "../../config.json";
+
+export default async function main() {
+  const simpleAccount = await Presets.Builder.SimpleAccount.init(
+    new ethers.Wallet(config.signingKey),
+    config.rpcUrl
+  );
+  const address = simpleAccount.getSender();
+
+  console.log(`SimpleAccount address: ${address}`);
+}
---
+++ scripts/simpleAccount/transfer.ts
@@ -0,0 +1,35 @@
+import { ethers } from "ethers";
+import { Client, Presets } from "userop";
+import { CLIOpts } from "../../src";
+// @ts-ignore
+import config from "../../config.json";
+
+export default async function main(t: string, amt: string, opts: CLIOpts) {
+  const paymasterMiddleware = opts.withPM
+    ? Presets.Middleware.verifyingPaymaster(
+        config.paymaster.rpcUrl,
+        config.paymaster.context
+      )
+    : undefined;
+  const simpleAccount = await Presets.Builder.SimpleAccount.init(
+    new ethers.Wallet(config.signingKey),
+    config.rpcUrl,
+    { paymasterMiddleware }
+  );
+  const client = await Client.init(config.rpcUrl);
+
+  const target = ethers.utils.getAddress(t);
+  const value = ethers.utils.parseEther(amt);
+  const res = await client.sendUserOperation(
+    simpleAccount.execute(target, value, "0x"),
+    {
+      dryRun: opts.dryRun,
+      onBuild: (op) => console.log("Signed UserOperation:", op),
+    }
+  );
+  console.log(`UserOpHash: ${res.userOpHash}`);
+
+  console.log("Waiting for transaction...");
+  const ev = await res.wait();
+  console.log(`Transaction hash: ${ev?.transactionHash ?? null}`);
+}
